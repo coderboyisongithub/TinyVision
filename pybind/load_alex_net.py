2f4f0db67929e5728dace2015d07cbc56dbce440
zzk2021 <zakarichang@outlook.com>
2025-07-18 19:45:19 +0800

    add init pybind

--- pybind/load_alex_net.py
+++ pybind/load_alex_net.py
@@ -8,7 +8,7 @@ from _module import Module
 class AlexNetMNIST(Module):
     def __init__(self, num_classes=10):
         super(AlexNetMNIST, self).__init__()
-        self.features = nn.Sequential([
+        self.features = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),  # 原始是11x11
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),  # 28x28 -> 14x14
@@ -21,16 +21,16 @@ class AlexNetMNIST(Module):
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)]  # 7x7 -> 3x3
+            nn.MaxPool2d(kernel_size=2, stride=2)  # 7x7 -> 3x3
         )
-        self.classifier = nn.Sequential([
+        self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 3 * 3, 4096),
             nn.ReLU(),
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(),
-            nn.Linear(4096, num_classes)] # 输出10类
+            nn.Linear(4096, num_classes) # 输出10类
         )
 
     def forward(self, x):
@@ -48,3 +48,6 @@ state_dict = alexnet.state_dict()
 p = AlexNetMNIST()
 p.summary()
 p.load_state_dict(state_dict)
+
+io = tt.Tensor.randn((1,1,28,28))
+p(io)
